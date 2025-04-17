c692e15529bdf42dcda000ca611525096c9d4beb
y.vasilenko <y.vasilenko@iva.ru>
2025-04-17 14:54:27 +0300

    fix:

--- internal/router/router.go
+++ internal/router/router.go
@@ -61,7 +61,7 @@ func NewApiRouter(controllers *modules.Controllers) *fiber.App {
 	r.Get("/api/v1/boost/:listing_id", controllers.Boost.GetBoost)
 
 	// images
-	r.Post("/api/v1/image", controllers.Image.UploadImage)
+	r.Post("/api/v1/image/upload", controllers.Image.UploadImage)
 	r.Get("/api/v1/image/:image_id", controllers.Image.GetImage)
 
 	return r
